3ff8314d87c901bfbd0e5f19c9d068514f3a9909
Liu <doan.quang.luu@tpiv.sun-asterisk.com>
2023-09-11 13:21:03 +0700

    [USER] Update giỏ hàng

--- home/urls.py
+++ home/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('category/',views.CategoryView.as_view(), name='category'),
     path('cart/',views.CartView.as_view(), name='cart'),
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('update_cart/', views.update_cart, name='update_cart'),
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('profile/', views.update_profile, name='profile'),
     path('custom_logout/', views.custom_logout, name='custom_logout'),
